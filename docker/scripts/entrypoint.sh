7e3516440423327ea1e396100b373ecc40f10737
davidmufc98 <davidmufc98@gmail.com>
2020-04-20 15:38:26 +0700

    add CI

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -15,18 +15,36 @@ npm install pm2@latest -g
 
 tee /app/${PROJECT_DIR}/ecosystem.config.js << EOF
 module.exports = {
-  apps : [{
-    name : "${APP_NAME}",
-    exec_mode : "cluster",
-    instances : 2,
-    script: "${SERVER_PATH_FILE}",
-    watch: true,
-    env: {
-      "HOST": "0.0.0.0",
-      "PORT": ${NODE_PORT},
-      "NODE_ENV": "production"
+  apps : [
+    {
+      name : "${PRODUCTION_APP}",
+      exec_mode : "cluster",
+      instances : 2,
+      script: "${SERVER_PATH_PRODUCTION}",
+      watch: true,
+      out_file: "/dev/null",
+      error_file: "/dev/null",
+      env: {
+        "HOST": "0.0.0.0",
+        "PORT": ${NODE_PORT},
+        "NODE_ENV": "production"
+      }
+    },
+    {
+      name : "${STAGING_APP}",
+      exec_mode : "cluster",
+      instances : 2,
+      script: "${SERVER_PATH_STAGING}",
+      watch: true,
+      out_file: "/dev/null",
+      error_file: "/dev/null",
+      env: {
+        "HOST": "0.0.0.0",
+        "PORT": ${NODE_PORT},
+        "NODE_ENV": "production"
+      }
     }
-  }]
+  ]
 };
 EOF
 
@@ -35,21 +53,22 @@ if [[ -d "/app/${PROJECT_DIR}" ]]
 then
 
   if [[ -d "/app/${PROJECT_DIR}/.nuxt" && -d "/app/${PROJECT_DIR}/node_modules" \
-    && -e "/app/${PROJECT_DIR}/package.json" && -e "/app/${PROJECT_DIR}/nuxt.config.js" \
-    && -d "/app/${PROJECT_DIR}/dist" && -e "/app/${PROJECT_DIR}/.env" ]]
+    && -e "/app/${PROJECT_DIR}/package.json" && -e "/app/${PROJECT_DIR}/nuxt.config.js" ]]
   then
     echo "Project ready for starting, please wait a moment set back and relax!"
     cd /app/${PROJECT_DIR}
     ls -al
+    npm install
+    npm run start:${BUILD_FLAG}
   else
     echo "Project not ready for starting, please check your project structure!"
     echo "Change directory to ${PROJECT_DIR}"
     cd /app/${PROJECT_DIR}
     echo "npm install"
     npm install
-    echo "npm run build"
-    npm run build
+    # npm run build-${BUILD_FLAG}
     ls -al
+    npm run start:${BUILD_FLAG}
   fi
 
 else 
